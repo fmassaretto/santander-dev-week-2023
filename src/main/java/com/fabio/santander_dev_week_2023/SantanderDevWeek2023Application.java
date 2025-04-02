aab348357c9e25be2cbe36d5e2468359f2ca28f9
Fabio Massaretto <fabio_no_more@yahoo.com.br>
2025-04-01 21:47:18 -0300

    Add OpenAPIDefinition annotation to fix CORS issue on railway

--- src/main/java/com/fabio/santander_dev_week_2023/SantanderDevWeek2023Application.java
+++ src/main/java/com/fabio/santander_dev_week_2023/SantanderDevWeek2023Application.java
@@ -1,9 +1,12 @@
 package com.fabio.santander_dev_week_2023;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class SantanderDevWeek2023Application {
 
 	public static void main(String[] args) {
